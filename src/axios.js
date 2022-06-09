5a0bf212093ddbc7188eba92b953e2e7c8abcb60
rushanthbala <rushanth9999@gmail.com>
2022-06-09 10:23:39 +0530

    change https

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://dcaapi.moodfor.codes",
+  baseURL: "https://dcaapi.moodfor.codes",
 
   withCredentials: false,
   headers: {
